43e67e391c61cfe3f1158450068d5b65c0981c03
Sameer Kumar <kumar.sam1612@gmail.com>
2023-08-13 14:41:44 +0530

    refactored sudoku code

--- codes/valid-sudoku.rb
+++ codes/valid-sudoku.rb
@@ -3,12 +3,25 @@
 # @param {Character[][]} board
 # @return {Boolean}
 def is_valid_sudoku(board)
-  transposed_board = board.transpose
+  uniq_rows?(board) && uniq_cols?(board) && uniq_blocks?(board)
+end
+
+def uniq_rows?(board)
+  9.times { |i| return false unless uniq_nums?(board[i]) }
+
+  true
+end
+
+def uniq_cols?(board)
+  board = board.transpose
+  9.times { |i| return false unless uniq_nums?(board[i]) }
+
+  true
+end
+
+def uniq_blocks?(board)
   i = 0
   j = 0
-
-  9.times { |i| return false unless uniq_nums?(board[i]) && uniq_nums?(transposed_board[i]) }
-
   while i < 9
     while j < 9
       values = [(0..2).map { |x| (0..2).map { |y| board[i + x][j + y] } }].flatten
